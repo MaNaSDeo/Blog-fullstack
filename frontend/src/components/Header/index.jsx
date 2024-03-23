3188bb529d823c88297f0cfaead455bd4ad238e4
MaNaSDeo <rmnslogin@gmail.com>
2024-03-24 00:09:58 +0530

    Header

---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,31 @@
+import styles from "./Header.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const logout = () => {
+    localStorage.clear();
+    alert("logout");
+  };
+
+  return (
+    <div className={styles.header}>
+      <div className={styles.header_blog_name}>Blog</div>
+      <nav className={styles.header_navigation}>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li onClick={logout}>Logout</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
