c96d931145b33360cd01e0311706cdc8e4cfa9d8
MaNaSDeo <rmnslogin@gmail.com>
2024-03-24 00:29:42 +0530

    Home

---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import PostView from "../../components/PostView/PostView";
+import styles from "./Home.module.scss";
+import axios from "axios";
+import { base_URL } from "../../config";
+
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await axios.get(`${base_URL}/posts`);
+    // console.log(response.data.posts);
+    setPosts(response.data.posts);
+  };
+
+  return (
+    <div className={styles.homePage}>
+      <main>
+        {posts.map((post) => {
+          return (
+            <PostView
+              key={post._id}
+              title={post.title}
+              content={post.content}
+              author={post.author}
+              publishedDate={post.createdAt}
+              updatedDate={post.updatedAt}
+            ></PostView>
+          );
+        })}
+      </main>
+    </div>
+  );
+};
+
+export default Home;
